7f4ebeb1d196b0ba1d201cd7951ffbef402433b6
mundi <christine.mundi@student.moringaschool.com$ git config --global user.email christine.mundi@student.moringaschool.com>
2023-05-26 12:36:32 +0300

    tests passed

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,11 @@
 import re
 
-my_pattern = r""
+my_pattern = r"(It's such a lovely day today\.|Some weather we're having today, huh\?|Maybe today's just not my day\.)"
 my_regex = re.compile(my_pattern)
 
+def test_matches_strings():
+    assert my_regex.fullmatch("It's such a lovely day today.")
+    assert my_regex.fullmatch("Some weather we're having today, huh?")
+    assert my_regex.fullmatch("Maybe today's just not my day.")
+
+test_matches_strings()
